d96c9280d22c608462d1c05a503402753ac7767c
0d-fako <od.fakorede@gmail.com>
2025-02-04 15:00:48 +0100

    Testing in Javascript

---
+++ Feb-2025/array.js
@@ -0,0 +1,10 @@
+const array = [];
+
+const arrayOfNumbers = new Array(2);
+arrayOfNumbers[0] = 1;
+arrayOfNumbers[1] = 2;
+arrayOfNumbers.push(1);
+arrayOfNumbers.push(2);
+
+console.log(arrayOfNumbers);
+
