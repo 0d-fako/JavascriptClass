09d54d89976f26e25fdc741d13e49cfe78099c8d
0d-fako <od.fakorede@gmail.com>
2025-02-11 10:34:55 +0100

    Testing in Javascript

---
+++ Feb-2025/objectManipulation.js
@@ -0,0 +1,18 @@
+const student = {
+    name: 'John',
+    age: 25,
+    hobbies: ['reading', 'coding', 'swimming'],
+    address: {
+        street: '123 Main St',
+        city: 'New York',
+        zip: 10001
+    }
+}
+
+function printStudentName(student) {
+    console.log(student.name);
+}
+
+function printStudentHobbies(student) {
+    console.log(student.hobbies);
+}
