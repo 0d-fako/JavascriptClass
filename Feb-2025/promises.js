7edb43f522dbedc757093e885a6dc52461f7e0f4
0d-fako <od.fakorede@gmail.com>
2025-02-19 19:30:51 +0100

    Promises

--- Feb-2025/promises.js
+++ Feb-2025/promises.js
@@ -7,7 +7,17 @@ const fetchPromise = fetch(
     .then((data) => {
       console.log(data[0].name);
     });
+
+    
+const anotherPromise = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+  );
   
+  anotherPromise
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data[0].name);
+    })
   
 
   const newPromise = fetch(
@@ -22,4 +32,33 @@ const fetchPromise = fetch(
       console.log(data);
     });
   });
+
+  const morePromises = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"); 
   
+
+    morePromises.then((response) => {
+        if(!response.ok){
+            throw new Error("HTTP error, status = " + response.status);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data[0].name);
+    })
+
+    const failedPromise = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+
+    failedPromise
+    .then((response) => {
+        if(!response.ok){
+            throw new Error("HTTP error, status = " + response.status);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data[0].name);
+    })
+    .catch((error) => {
+        console.log(error);
+    })  
+    
