7ec54137bc20c37d85c9bab32c4a4a26c18f5630
0d-fako <od.fakorede@gmail.com>
2025-02-19 10:37:05 +0100

    Array Tasks

---
+++ Feb-2025/enum.js
@@ -0,0 +1,13 @@
+const Colors = Object.freeze({
+    RED: "red",
+    GREEN: "green",
+    BLUE: "blue",
+  });
+  
+  
+  const favoriteColor = Colors.RED;
+  
+  if (favoriteColor === Colors.RED) {
+    console.log("You like red!");
+  }
+  
