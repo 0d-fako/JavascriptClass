6bfa8e4e737a72704f54d66a7a4de2d7f732db5e
0d-fako <od.fakorede@gmail.com>
2025-02-20 03:28:31 +0100

    Text Book Exercises

--- TextBookExercises/higherOrderFunction.js/functions.js
+++ TextBookExercises/higherOrderFunction.js/functions.js
@@ -7,10 +7,25 @@ function range (start , end){
     return array;
 }
 
+console.log(range(1,10))
 
 function sum(array){
-    return array.reduce((total, num) => total + num, 0);
+    return array.reduce((total, num) => total + num);
 }
 
 
 console.log(sum(range(1,10)))
+
+
+function stringWithLongestLength(array){ 
+    let longest = "";
+    for(let i = 0; i< array.length; i++){
+        if (array[i].length > longest.length){
+            longest = array[i];
+        }
+    }   
+    return longest;
+}
+
+
+console.log(stringWithLongestLength(["apple", "banana", "orange", "kiwi"]))
