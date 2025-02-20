53c15a951f3161f8f9cceae4da586bae7fefe50a
0d-fako <od.fakorede@gmail.com>
2025-02-20 02:44:15 +0100

    Text Book Exercises

---
+++ TextBookExercises/ch2/controlFlow.js
@@ -0,0 +1,11 @@
+let num = Number(prompt("Enter a number: "));
+
+
+if (num < 10){
+    console.log("The number is less than 10");
+
+} else if (num < 100){
+    console.log("The number is less than 100");
+} else {
+    console.log("The number is greater than or equal to 100");
+}
