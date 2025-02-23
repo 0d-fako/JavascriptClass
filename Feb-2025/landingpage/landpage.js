7112189473d59f0f5c2590d705dbc63ef5c0a997
0d-fako <od.fakorede@gmail.com>
2025-02-23 15:33:54 +0100

    Landing page

---
+++ Feb-2025/landingpage/landpage.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const button = document.querySelector("button");
+
+
+    button.addEventListener("click", function() {
+        alert("Button clicked! More content coming soon...");
+    });
+});
