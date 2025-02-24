8d0c71d0788b63c4beabd9f08701195e6f4fe7ca
0d-fako <od.fakorede@gmail.com>
2025-02-24 14:48:55 +0100

    Landing page

--- Feb-2025/landingpage/landpage.js
+++ Feb-2025/landingpage/landpage.js
@@ -1,7 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     
-    const button = document.querySelector("button");
+    // const button = document.querySelector("button");
 
     const hamburger = document.querySelector('.hamburger');
     const navContainer = document.querySelector('.nav-container');
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
         hamburger.classList.toggle('active');
         navContainer.classList.toggle('active');
     });
-    
+
     button.addEventListener("click", function() {
         alert("Button clicked! More content coming soon...");
     });
