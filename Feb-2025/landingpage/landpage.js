e3277a3278a745d89bb5775ad49bc98d0a4d1009
0d-fako <od.fakorede@gmail.com>
2025-02-24 08:40:29 +0100

    Landing page

--- Feb-2025/landingpage/landpage.js
+++ Feb-2025/landingpage/landpage.js
@@ -3,7 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
     
     const button = document.querySelector("button");
 
-
+    const hamburger = document.querySelector('.hamburger');
+    const navContainer = document.querySelector('.nav-container');
+    
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navContainer.classList.toggle('active');
+    });
+    
     button.addEventListener("click", function() {
         alert("Button clicked! More content coming soon...");
     });
