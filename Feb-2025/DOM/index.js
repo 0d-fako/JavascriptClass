0c43775e034bf1f1460a3b8d5fd825220ab5a2a4
0d-fako <od.fakorede@gmail.com>
2025-02-25 11:50:01 +0100

    Dom

--- Feb-2025/DOM/index.js
+++ Feb-2025/DOM/index.js
@@ -1,1 +1,9 @@
 const idWrapper = document.getElementById('wrapper');
+
+console.log(idWrapper);
+
+const pageBanner = document.getElementById('page-banner');
+console.log(pageBanner);
+
+const classTitle = document.getElementsByClassName('title');
+console.log(classTitle);
