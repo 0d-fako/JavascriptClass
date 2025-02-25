30529263812244cdd124a3642c7e9871eb288ebe
0d-fako <od.fakorede@gmail.com>
2025-02-25 20:47:37 +0100

    Lumi Portfolio React

---
+++ Feb-2025/lumi-portfolio/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section id="about">
+            <h2>About Me</h2>
+            <p>
+                I am a full-stack web developer based in Lagos, Nigeria. I specialize in building responsive, accessible websites and web applications using modern technologies.
+            </p>
+        </section>
+    )
+}
+
+export default About;
---
+++ Feb-2025/lumi-portfolio/src/components/Project.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Project = () => {
+    return (
+        <section id="projects">
+            <h2>Projects</h2>
+            <ul>
+                <li>
+                    <h3>Project 1</h3>
+                    <p>
+                        E-Commerce Backend in Java
+                    </p>
+                    <a href ="https://github.com/0d-fako/ecommerce_backend">Github</a>
+                </li>
+                <li>
+                    <h3>Project 2</h3>
+                    <p>
+                        This is another project I worked on recently. It involved building a web application for a client using Node.js and Express.
+                    </p>
+                    <a href ="https://github.com/0d-fako/ecommerce_backend">Github</a>
+                </li>
+                <li>
+                    <h3>Project 3</h3>
+                    <p>
+                        This is a personal project I worked on recently. It involved building a portfolio website for myself using HTML, CSS, and JavaScript.
+                    </p>
+                    <a href ="https://github.com/0d-fako/ecommerce_backend">Github</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
+
+
+
+export default Project;
