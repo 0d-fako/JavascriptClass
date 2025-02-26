990d5c18941bf6c741965c39460f3e68b0682fc7
0d-fako <od.fakorede@gmail.com>
2025-02-26 08:48:10 +0100

    Lumi Portfolio React

---
+++ Feb-2025/lumi-portfolio/src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Contact = () => {
+    return (
+        <section id="contact">
+            <h2>Contact</h2>
+            <p>
+                You can reach me via email at: od.fakorede@gmail.com
+            </p>
+        </section>  
+    )
+}
+
+
+export default Contact;
