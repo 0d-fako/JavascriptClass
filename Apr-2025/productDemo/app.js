1d5736e1f5bf5b9d52846a242cccbfcc2d1f4c20
0d-fako <od.fakorede@gmail.com>
2025-04-17 14:43:27 +0100

    App. js file for  HTML

--- Apr-2025/productDemo/app.js
+++ Apr-2025/productDemo/app.js
@@ -1,16 +1,27 @@
 const PRODUCT_URL = "https://fakestoreapi.com/products";
 
-const productsContainer = document.querySelector(".products");
+const productsContainer = document.querySelector(".productsWrapper");
 
 console.log(productsContainer);
 
-const getProducts = (url)=>{
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => {console.log(data)})
-    .catch((error) => console.log(error))
-};
+// const getProducts = (url)=>{
+//     fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {console.log(data)})
+//     .catch((error) => console.log(error))
+// };
 
+
+
+const getProducts = async (url) => {
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch(error){
+        console.log(error);
+    }
+}
 
 
 getProducts(PRODUCT_URL);
