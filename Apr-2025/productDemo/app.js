44f0467b75971fd2ddd7cecaf887da8d80b2eb43
0d-fako <od.fakorede@gmail.com>
2025-04-17 15:04:02 +0100

    App. js file for  HTML

--- Apr-2025/productDemo/app.js
+++ Apr-2025/productDemo/app.js
@@ -18,6 +18,8 @@ const getProducts = async (url) => {
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
+        displayProducts(data);
+    
     } catch(error){
         console.log(error);
     }
@@ -28,5 +30,29 @@ getProducts(PRODUCT_URL);
 
 
 function displayProducts(products){
+    products.forEach((product) => {
+        const {title, price, description, image} = product;
+        console.log(title, price, description, image);
+
+        const productElement = document.createElement('div');
+        productElement.classList.add('product');
+        
+        
+        const nairaPrice = price * 1500;
+        
+        
+        productElement.innerHTML = `
+            <img src="${image}" alt="${title}">
+            <h3 class="productName">${title}</h3>
+            <p class="productDescription">${description.substring(0, 100)}...</p>
+            <p class="productPrice">&#8358; ${nairaPrice.toFixed(2)}</p>
+            <button class="addToCartButton">Add to Cart</button>
+        `;
+        
+    
+        productsContainer.appendChild(productElement);
+    })
+
+
 
 }
