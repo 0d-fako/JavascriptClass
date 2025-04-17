28eb744602e7202ef16cc999c5d13cfbbb770e44
0d-fako <od.fakorede@gmail.com>
2025-04-17 14:05:57 +0100

    App. js file for  HTML

--- Apr-2025/productDemo/app.js
+++ Apr-2025/productDemo/app.js
@@ -1,10 +1,18 @@
 const PRODUCT_URL = "https://fakestoreapi.com/products";
 
 
-const getProducts = ()=>{
-
+const getProducts = (url)=>{
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {console.log(data)})
+    .catch((error) => console.log(error))
 };
 
 
 
 getProducts(PRODUCT_URL);
+
+
+function displayProducts(){
+    
+}
