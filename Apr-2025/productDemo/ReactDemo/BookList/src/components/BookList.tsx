86eae993c170fd3b7b10e300dd8748b1296108fa
0d-fako <od.fakorede@gmail.com>
2025-04-23 14:07:04 +0100

    React Demo

---
+++ Apr-2025/productDemo/ReactDemo/BookList/src/components/BookList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const BookList = () => {
+    return (
+        <div>
+            <h1>Book List</h1>
+            <ul>
+                <li>Book 1</li>
+                <li>Book 2</li>
+                <li>Book 3</li>
+            </ul>
+        </div>
+    )
+}   
