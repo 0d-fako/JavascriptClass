491ea7dd6d2580579d9c49e101e0efe745f1922c
0d-fako <od.fakorede@gmail.com>
2025-04-23 15:46:04 +0100

    Updated CSS

--- Apr-2025/productDemo/ReactDemo/BookList/src/components/BookList.tsx
+++ Apr-2025/productDemo/ReactDemo/BookList/src/components/BookList.tsx
@@ -1,40 +1,40 @@
-import React from 'react'
+// import React from 'react'
 import style from './bookList.module.css'
 
 const BookList = () => {
     return (
         <div id="wrapper">
 	    <header>
-	    	<div id="page-banner">
+			<div id={style['page-banner']}>
 				<h1 className="title"> Book Collections</h1>
           <p>Books</p>
-          <form id="search-books">
+          <form id={style['search-books']}>
             <input type="text" placeholder="Search books..." />
           </form>
 	    	</div>
 	    </header>
-	    <div id="book-list">
-	    	<h2 className="title">Books to Read</h2>
+	    <div id={style['book-list']}>
+	    	<h2 className={style["title"]}>Books to Read</h2>
 	    	<ul>
 	    		<li>
-	    			<span className="name">Name of the Wind</span>
-	    			<span className="delete">delete</span>
+	    			<span className={style["name"]}>Name of the Wind</span>
+	    			<span className={style['delete']}>delete</span>
 	    		</li>
 	    		<li>
-	    			<span className="name">The Wise Man's Fear</span>
-	    			<span className="delete">delete</span>
+	    			<span className={style["name"]}>The Wise Man's Fear</span>
+	    			<span className={style['delete']}>delete</span>
 	    		</li>
 	    		<li>
-	    			<span className="name">Kafka on the Shore</span>
-	    			<span className="delete">delete</span>
+	    			<span className={style["name"]}>Kafka on the Shore</span>
+	    			<span className={style['delete']}>delete</span>
 	    		</li>
 	    		<li>
-	    			<span className="name">The Master and the Margarita</span>
-	    			<span className="delete">delete</span>
+	    			<span className={style["name"]}>The Master and the Margarita</span>
+	    			<span className={style['delete']}>delete</span>
 	    		</li>
 	    	</ul>
 	    </div>
-	    <form id="add-book">
+	    <form id={style["add-book"]}>
 	    	<input type="text" placeholder="Add a book..." />
 			<button type="submit" aria-label="Add Button">Add</button>
 	    </form>
