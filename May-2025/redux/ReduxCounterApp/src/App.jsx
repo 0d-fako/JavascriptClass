5ea192ffe35b03a1e1c1feb2d76a980c63acd97a
0d-fako <od.fakorede@gmail.com>
2025-05-05 14:19:44 +0100

    Redux Counter update

--- May-2025/redux/ReduxCounterApp/src/App.jsx
+++ May-2025/redux/ReduxCounterApp/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 
+import Counter from './features/counter/Counter'
+
 function App() {
   
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <Counter/>
     </>
   )
 }
---
+++ May-2025/redux/ReduxCounterApp/src/features/counter/Counter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Counter =() => {
+    return (
+        <div>
+            <h1>Counter</h1>
+            <h2>0</h2>
+            <div>
+                <button>Increment</button>
+                <button>Decrement</button>
+            </div>
+
+        </div>
+    )      
+}
+
+
+export default Counter;
