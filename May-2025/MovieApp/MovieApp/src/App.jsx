e2307d904a58d90a19366c13ff5ca01159b5c192
0d-fako <od.fakorede@gmail.com>
2025-05-09 09:49:28 +0100

    Updated Students

--- May-2025/MovieApp/MovieApp/src/App.jsx
+++ May-2025/MovieApp/MovieApp/src/App.jsx
@@ -1,21 +1,17 @@
-import SignUp from './component/auth/signUp/signUp'
-import Login from './component/auth/login/login'
-import './App.css'
-import {Route, Routes} from 'react-router-dom'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./component/auth/signUp/signUp";
+import Login from "./component/auth/login/login";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
+    <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-      <SignUp />
-      
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
