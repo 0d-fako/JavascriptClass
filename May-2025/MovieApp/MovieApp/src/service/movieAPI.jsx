603d7c0500a0bada26fca26d61502601d2a37fb2
0d-fako <od.fakorede@gmail.com>
2025-05-14 11:25:29 +0100

    Defined Movie APi

--- May-2025/MovieApp/MovieApp/src/service/movieAPI.jsx
+++ May-2025/MovieApp/MovieApp/src/service/movieAPI.jsx
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const movieAPIKey = import.meta.env.VITE_MOVIE_API_KEY;
+const movieUrl = "https://api.themoviedb.org/3/movie";
+
 if (!movieAPIKey) {
   throw new Error("VITE_MOVIE_API_KEY is not defined");
 }
@@ -8,27 +10,39 @@ if (!movieAPIKey) {
 const movieAPI = createApi({
   reducerPath: "movieAPI",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.themoviedb.org/3",
-    prepareHeaders: (headers) => {
-      headers.set("Authorization", `Bearer ${process.env.VITE_MOVIE_API_KEY}`);
-      return headers;
-    },
+    baseUrl: `${movieUrl}`,
   }),
   endpoints: (builder) => ({
     getMovies: builder.query({
-      query: () => "/movie/popular",
-    }),
-    getMovieDetails: builder.query({
-      query: (id) => `/movie/${id}`,
-    }),
-    getMovieReviews: builder.query({
-      query: (id) => `/movie/${id}/reviews`,
+      query: () => "/now_playing",
     }),
   }),
 });
 
-export const {
-  useGetMoviesQuery,
-  useGetMovieDetailsQuery,
-  useGetMovieReviewsQuery,
-} = movieAPI;
+// const movieAPI = createApi({
+//   reducerPath: "movieAPI",
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: "https://api.themoviedb.org/3",
+//     prepareHeaders: (headers) => {
+//       headers.set("Authorization", `Bearer ${process.env.VITE_MOVIE_API_KEY}`);
+//       return headers;
+//     },
+//   }),
+//   endpoints: (builder) => ({
+//     getMovies: builder.query({
+//       query: () => "/movie/popular",
+//     }),
+//     getMovieDetails: builder.query({
+//       query: (id) => `/movie/${id}`,
+//     }),
+//     getMovieReviews: builder.query({
+//       query: (id) => `/movie/${id}/reviews`,
+//     }),
+//   }),
+// });
+
+// export const {
+//   useGetMoviesQuery,
+//   useGetMovieDetailsQuery,
+//   useGetMovieReviewsQuery,
+// } = movieAPI;
