23d50e35046d4cddf11a4dadc4e9c13723666e6f
0d-fako <od.fakorede@gmail.com>
2025-05-14 12:12:08 +0100

    Movie API

--- May-2025/MovieApp/MovieApp/src/component/movies/Movies.jsx
+++ May-2025/MovieApp/MovieApp/src/component/movies/Movies.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { useGetMovies } from "../../service/movieAPI";
+import MovieCard from "../../reuseable/MovieCar";
 
 const Movies = () => {
   const data = useGetMovies();
@@ -10,7 +11,9 @@ const Movies = () => {
   return (
     <div>
       <h1>Movies</h1>
-      <p>List of movies will be displayed here.</p>
+      <MovieCard data={data} />
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error loading movies</p>}
     </div>
   );
 };
